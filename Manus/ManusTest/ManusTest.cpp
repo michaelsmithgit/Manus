d6aa97c5f1a3f179e9b025cb1a1828d1f96f7d29
Jules Blok <jules.blok@gmail.com>
2015-01-15 20:41:45 +0100

    ManusTest: Add testing sample for the Manus SDK.

---
+++ Manus/ManusTest/ManusTest.cpp
@@ -0,0 +1,32 @@
+// ManusTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Manus.h"
+
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ManusInit();
+
+	int last_packet = 0;
+	while (true)
+	{
+		for (int i = 0; i < ManusGetGloveCount(); i++)
+		{
+			GLOVE_STATE state;
+			ManusGetState(i, &state);
+			if (state.PacketNumber != last_packet)
+			{
+				last_packet = state.PacketNumber;
+				printf("%d;%d;%d;%d\n", state.data.Quaternion[0], state.data.Quaternion[1], state.data.Quaternion[2], state.data.Quaternion[3]);
+			}
+		}
+	}
+
+	ManusExit();
+
+	return 0;
+}
+

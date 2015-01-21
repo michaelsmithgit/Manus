f6adc9f3cc0eb8e15c2060766b4e42b98b5dbc96
Jules Blok <jules.blok@gmail.com>
2015-01-21 19:04:30 +0100

    ManusTest: Don't check last packet.

--- Manus/ManusTest/ManusTest.cpp
+++ Manus/ManusTest/ManusTest.cpp
@@ -10,20 +10,15 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	ManusInit();
 
-	int last_packet = 0;
 	while (true)
 	{
 		for (int i = 0; i < ManusGetGloveCount(); i++)
 		{
 			GLOVE_STATE state;
 			ManusGetState(i, &state, true);
-			if (state.PacketNumber != last_packet)
-			{
-				last_packet = state.PacketNumber;
-				printf("pack#: %d\n", state.PacketNumber);
-				printf("quats: %f;%f;%f;%f\n", state.data.Quaternion.x, state.data.Quaternion.y, state.data.Quaternion.z, state.data.Quaternion.w);
-				printf("euler: %f;%f;%f\n", state.data.Angles.x, state.data.Angles.y, state.data.Angles.z);
-			}
+			printf("glove: %d - %d\n", i, state.PacketNumber);
+			printf("quats: %f;%f;%f;%f\n", state.data.Quaternion.x, state.data.Quaternion.y, state.data.Quaternion.z, state.data.Quaternion.w);
+			printf("euler: %f;%f;%f\n", state.data.Angles.x, state.data.Angles.y, state.data.Angles.z);
 		}
 	}
 

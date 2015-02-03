b850e7bd441b568e122bcb7d2c04c352c53ef815
Jules Blok <jules.blok@gmail.com>
2015-02-04 00:25:23 +0100

    Release the Manus SDK under LGPL.

--- Manus/ManusTest/targetver.h
+++ Manus/ManusTest/targetver.h
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2015 Manus Machina
+ *
+ * This file is part of the Manus SDK.
+ * 
+ * Manus SDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Manus SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Manus SDK. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 // Including SDKDDKVer.h defines the highest available Windows platform.
